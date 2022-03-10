9084a7df90cfef0369f210e0252d32631dcb1be9
jasarard <jasarard@students.zhaw.ch>
2022-03-10 12:34:48 +0100

    MoveListStrategy angefangen, Input und Output ergänzt für MoveListStrategy

--- src/main/java/ch/zhaw/pm2/racetrack/strategy/MoveListStrategy.java
+++ src/main/java/ch/zhaw/pm2/racetrack/strategy/MoveListStrategy.java
@@ -1,13 +1,38 @@
 package ch.zhaw.pm2.racetrack.strategy;
 
 import ch.zhaw.pm2.racetrack.PositionVector.Direction;
+import ch.zhaw.pm2.racetrack.logic.Config;
+import ch.zhaw.pm2.racetrack.ui.Input;
+import ch.zhaw.pm2.racetrack.ui.Output;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MoveListStrategy implements MoveStrategy {
+    private final Config config;
+    private final Output output;
+    private final Input input;
+    private final File moveFile;
 
     private List<Direction> directions;
+
+    public MoveListStrategy(){
+        config = new Config();
+        moveFile = selectMoveListFile();
+        readAllDirections(moveFile);
+    }
+
+    private File selectMoveListFile(){
+        File moveDirectory = config.getMoveDirectory();
+        output.outputMoveList(moveDirectory);
+        input.getSelectedMoveFile(moveDirectory);
+    }
+
+    private void readAllDirections(){
+        
+    }
+
     @Override
     public Direction nextMove() {
         // TODO: implementation
