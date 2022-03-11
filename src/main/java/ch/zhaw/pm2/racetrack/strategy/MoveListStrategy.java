e7fb2c61a411a75ae49f19f97ed184da68cda92a
jasarard <jasarard@students.zhaw.ch>
2022-03-11 10:11:08 +0100

    nextMove() method in MoveListStrategy improved

--- src/main/java/ch/zhaw/pm2/racetrack/strategy/MoveListStrategy.java
+++ src/main/java/ch/zhaw/pm2/racetrack/strategy/MoveListStrategy.java
@@ -56,6 +56,8 @@ public class MoveListStrategy implements MoveStrategy {
             Direction currentMove = movesList.get(currentMoveIndex);
             ++currentMoveIndex;
             return currentMove;
+        } else{
+            return Direction.NONE;
         }
     }
 }
