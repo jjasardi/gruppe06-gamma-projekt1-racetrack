2a2b9dfbae736d4e94cb1bb0934c0ba8def009ea
gabricyr <gabricyr@students.zhaw.ch>
2022-03-17 10:50:57 +0100

    exception handling

--- src/main/java/ch/zhaw/pm2/racetrack/InvalidFileFormatException.java
+++ src/main/java/ch/zhaw/pm2/racetrack/InvalidFileFormatException.java
@@ -1,12 +1,11 @@
 package ch.zhaw.pm2.racetrack;
 
-import java.io.Serial;
 
 /**
  * 
  */
 public class InvalidFileFormatException extends Exception {
-    @Serial
+
     private static final long serialVersionUID = 7421314400704183413L;
 
     /**
--- src/main/java/ch/zhaw/pm2/racetrack/InvalidTrackFormatException.java
+++ src/main/java/ch/zhaw/pm2/racetrack/InvalidTrackFormatException.java
@@ -1,9 +1,10 @@
 package ch.zhaw.pm2.racetrack;
 
-import java.io.Serial;
-
+/**
+ *
+ */
 public class InvalidTrackFormatException extends Exception {
-    @Serial
+
     private static final long serialVersionUID = 762037463419578740L;
 
     /**
