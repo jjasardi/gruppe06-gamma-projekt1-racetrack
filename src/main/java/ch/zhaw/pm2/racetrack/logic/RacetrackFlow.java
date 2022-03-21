92e5af357bec489d13ba7857fec0b63cfa494ab0
jasarard <jasarard@students.zhaw.ch>
2022-03-21 16:00:14 +0100

    started implementation racetrackflow

--- src/main/java/ch/zhaw/pm2/racetrack/logic/RacetrackFlow.java
+++ src/main/java/ch/zhaw/pm2/racetrack/logic/RacetrackFlow.java
@@ -1,5 +1,7 @@
 package ch.zhaw.pm2.racetrack.logic;
 
+import ch.zhaw.pm2.racetrack.Car;
+import ch.zhaw.pm2.racetrack.Track;
 import ch.zhaw.pm2.racetrack.exceptions.TracklistEmptyException;
 import ch.zhaw.pm2.racetrack.given.ConfigSpecification;
 import ch.zhaw.pm2.racetrack.ui.Input;
@@ -24,18 +26,42 @@ public class RacetrackFlow {
         config = new Config();
         output = new Output();
         input = new Input(output, config);
+        startGame();
     }
-
 
     /**
      * @throws TracklistEmptyException
      */
-    public void startGame() throws TracklistEmptyException {
+    private void startGame() {
+        setup();
+    }
+
+    private void setup() {
+        initializeGame();
+        setStrategies();
         output.welcomeToRacetrack();
         output.outputTrackList(config.getTrackDirectory());
         File selectedTrackFile = input.getSelectedTrackFile(config.getTrackDirectory());
-        //game = new Game();
+        Track track = new Track(selectedTrackFile);
+        game = new Game(track, track.getCarCount());
+        for (Car car : game.getTrack().getCars()) {
+            output.outputStrategyTypes(ConfigSpecification.StrategyType.values());
+            car.setMoveStrategy(input.getSelectedMoveStrategy());
+        }
+    }
 
-        output.outputStrategyTypes(ConfigSpecification.StrategyType.values());
+    private void initializeGame() {
+        output.welcomeToRacetrack();
+        output.outputTrackList(config.getTrackDirectory());
+        File selectedTrackFile = input.getSelectedTrackFile(config.getTrackDirectory());
+        Track track = new Track(selectedTrackFile);
+        game = new Game(track);
+    }
+
+    private void setStrategies() {
+        for (Car car : game.getTrack().getCars()) {
+            output.outputStrategyTypes(ConfigSpecification.StrategyType.values());
+            car.setMoveStrategy(input.getSelectedMoveStrategy());
+        }
     }
 }
