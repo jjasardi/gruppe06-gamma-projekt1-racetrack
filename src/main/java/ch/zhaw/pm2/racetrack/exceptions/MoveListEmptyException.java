256f288d491b6526db99adb5b1b224ed3b0df6f6
Yohan Unver <yohan.unver@gmail.com>
2022-03-25 09:17:14 +0100

    added message in own exceptions

--- src/main/java/ch/zhaw/pm2/racetrack/exceptions/MoveListEmptyException.java
+++ src/main/java/ch/zhaw/pm2/racetrack/exceptions/MoveListEmptyException.java
@@ -5,4 +5,11 @@ package ch.zhaw.pm2.racetrack.exceptions;
  */
 public class MoveListEmptyException extends Exception {
     private static final long serialVersionUID = 1871145064442655706L;
+
+    /**
+     * @param errorMessage
+     */
+    public MoveListEmptyException(String errorMessage) {
+        super(errorMessage);
+    }
 }
--- src/main/java/ch/zhaw/pm2/racetrack/exceptions/TracklistEmptyException.java
+++ src/main/java/ch/zhaw/pm2/racetrack/exceptions/TracklistEmptyException.java
@@ -5,4 +5,11 @@ package ch.zhaw.pm2.racetrack.exceptions;
  */
 public class TracklistEmptyException extends Exception {
     private static final long serialVersionUID = 8333293512238364852L;
+
+    /**
+     * @param errorMessage
+     */
+    public TracklistEmptyException(String errorMessage) {
+        super(errorMessage);
+    }
 }
